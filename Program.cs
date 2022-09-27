e6fd5a28bf47db5fd00950e77a1335d6407b86e3
Emelie Strömberg <emelie_3rnst@hotmail.com>
2022-09-27 14:54:55 +0200

    First launch

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TränaKlasser;
+
+    internal class Program
+    {
+        internal static void Main(string[] args)    
+        {
+                HundDagis hund = new HundDagis();
+            
+                Console.WriteLine("Vänligen skriv hundens namn");
+                hund.dogName = Console.ReadLine();
+
+                Console.WriteLine("Vänligen skriv hundens ålder");
+                hund.dogAge = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Vänligen skriv hundens ras");
+                hund.dogBreed = Console.ReadLine();
+
+                Console.WriteLine("Vänligen skriv hundens ägare namn");
+                hund.dogOwnerName = Console.ReadLine();
+            
+        }
+    }
+
---
+++ hunddagis Klass.cs
@@ -0,0 +1,21 @@
+namespace TränaKlasser;
+
+    class HundDagis
+    {
+        public string dogName;
+        public int dogAge;
+        public string dogBreed;
+        public string dogOwnerName;
+
+        public HundDagis()
+        {
+
+        }
+        public HundDagis(string name, int age, string breed, string owner)
+        {
+           name = dogName;
+           age = dogAge;
+           breed = dogBreed;
+           owner = dogOwnerName;
+        }
+    }
